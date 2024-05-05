14352ae8f112b9591e71570391a42c87d6724c0f
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2024-05-05 12:17:24 +0600

    function with generic

--- mission-1/module-2/genericsType.ts
+++ mission-1/module-2/genericsType.ts
@@ -1,25 +1,35 @@
 {
-// generic type 
+  // generic type
 
-type GenericType<T> = Array<T>
+  type GenericType<T> = Array<T>;
 
-const numbers : GenericType<number>=[1,2,3,4,5,55] //pass type like arguments
+  const numbers: GenericType<number> = [1, 2, 3, 4, 5, 55]; //pass type like arguments
 
-const users:GenericType<{name:string,id:number}> =[
+  const users: GenericType<{ name: string; id: number }> = [
     {
-        name:'n',
-        id:1
+      name: "n",
+      id: 1,
     },
     {
-        name:'f',
-        id:2
+      name: "f",
+      id: 2,
     },
-]
+  ];
 
-//generic tuple
-type GenericTuple<X,Y>=[X,Y]
+  //generic tuple
+  type GenericTuple<X, Y> = [X, Y];
 
-const peoples :GenericTuple<string,number> =['name',65]
+  const peoples: GenericTuple<string, number> = ["name", 65];
 
+  //function with generic
+  const createFunctionGeneric = <T>(arg: T): T[] => {
+    return [arg];
+  };
 
+  createFunctionGeneric<string>("");
+  createFunctionGeneric<number>(2354);
+  createFunctionGeneric<{ name: string; id: number }>({
+    name: "afvd",
+    id: 874,
+  });
 }
