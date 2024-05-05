93ab540c82ee254148d94cc0e37372e03d177cd4
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2024-05-05 12:43:38 +0600

    constrain using keyof

--- mission-1/module-2/constraints.ts
+++ mission-1/module-2/constraints.ts
@@ -1,12 +1,25 @@
 {
-    //constrain means which property must be included
-     const addStudent =<T extends {id:number}>(obj:T)=>{
-        const courseName='L2'
-        return {
-            ...obj,
-            courseName
-        }
-     }
+  //constrain means which property must be included
+  const addStudent = <T extends { id: number }>(obj: T) => {
+    const courseName = "L2";
+    return {
+      ...obj,
+      courseName,
+    };
+  };
 
-     addStudent<{id:number,name:string}>({name:'nahid',id:76})
+  addStudent<{ id: number; name: string }>({ name: "nahid", id: 76 });
+
+  //  constrain using keyof
+
+  const getPropertyValue =<O,K extends keyof O> (obj:O, k:K) => {
+    return obj[k];
+  };
+
+  const userr = {
+    name: "nahsd",
+    id: 8675,
+  };
+
+  getPropertyValue(userr,'name')
 }
